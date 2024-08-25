1c70d3627102d16cb102a4153bfa0f42dcad8261
d3m1s4m <d3m1s4m@gmail.com>
2024-08-25 23:43:19 +0330

    add export as json action to block & follow relation admin

--- relations/admin.py
+++ relations/admin.py
@@ -5,6 +5,7 @@ from .models import FollowRelation, BlockRelation
 
 @admin.register(FollowRelation)
 class FollowRelationAdmin(admin.ModelAdmin):
+    actions = ('export_as_json',)
     autocomplete_fields = ('from_user', 'to_user')
     list_display = ('from_user', 'to_user', 'is_accepted')
     list_display_links = ('from_user', 'to_user')
@@ -12,11 +13,30 @@ class FollowRelationAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ('from_user__username__istartswith', 'to_user__username__istartswith')
 
+    @admin.action(description='Export selected follow relations as JSON')
+    def export_as_json(self, request, queryset):
+        from django.http import JsonResponse
+
+        data = list(queryset.values('from_user__username', 'to_user__username', 'is_accepted'))
+        response = JsonResponse(data, safe=False)
+        response['Content-Disposition'] = 'attachment; filename="follow_relations.json"'
+        return response
+
 
 @admin.register(BlockRelation)
 class BlockRelationAdmin(admin.ModelAdmin):
+    actions = ('export_as_json',)
     autocomplete_fields = ('blocker', 'blocked')
     list_display = ('blocker', 'blocked')
     list_display_links = ('blocker', 'blocked')
     list_per_page = 10
     search_fields = ('blocker__username__istartswith', 'blocked__username__istartswith')
+
+    @admin.action(description='Export selected block relations as JSON')
+    def export_as_json(self, request, queryset):
+        from django.http import JsonResponse
+
+        data = list(queryset.values('blocker', 'blocked'))
+        response = JsonResponse(data, safe=False)
+        response['Content-Disposition'] = 'attachment; filename="block_relations.json"'
+        return response
