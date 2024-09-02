eeaedbbfe19ac79bb6862d7d802ecc8bdcab45a6
d3m1s4m <d3m1s4m@gmail.com>
2024-09-02 21:23:38 +0330

    add admin for notifications app

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
-# Register your models here.
+from notifications.models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    actions = ('mark_as_read', 'mark_as_unread', 'export_as_json')
+    autocomplete_fields = ('receiver', 'sender', 'post')
+    list_display = ('receiver', 'sender', 'is_read', 'notification_type')
+    list_editable = ('is_read',)
+    list_filter = ('is_read', 'notification_type')
+    list_per_page = 10
+    search_fields = ('receiver__username__istartswith',)
+
+    @admin.action(description='Export selected notifications as JSON')
+    def export_as_json(self, request, queryset):
+        from django.http import JsonResponse
+
+        data = list(queryset.values())
+        response = JsonResponse(data, safe=False)
+        response['Content-Disposition'] = 'attachment; filename="notifications.json"'
+        return response
+
+    @admin.action(description='Mark selected notifications as read')
+    def mark_as_read(self, request, queryset):
+        updated_count = queryset.update(is_read=True)
+        self.message_user(
+            request,
+            f'Successfully marked {updated_count} notifications as read.',
+            messages.SUCCESS
+        )
+
+    @admin.action(description='Mark selected notifications as unread')
+    def mark_as_unread(self, request, queryset):
+        updated_count = queryset.update(is_read=False)
+        self.message_user(
+            request,
+            f'Successfully marked {updated_count} notifications as unread.',
+            messages.SUCCESS
+        )
+
+
+
