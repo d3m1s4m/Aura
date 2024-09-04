e3c03e0e7d7254a3ac89368efa722b7a5d3da5fc
d3m1s4m <d3m1s4m@gmail.com>
2024-09-04 10:59:10 +0330

    add filter to LocationViewSet

--- locations/views.py
+++ locations/views.py
@@ -8,4 +8,8 @@ from locations.serializers import LocationSerializer
 class LocationViewSet(ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+
+    ordering = ("-created_at",)
+    ordering_fields = ("created_at",)
     permission_classes = (IsAdminUser,)
+    search_fields = ('name__istartswith', 'lat', 'long')
